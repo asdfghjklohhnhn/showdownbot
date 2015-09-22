0849a1f8c566a059df4f7a2bea996f133ac9d71e
asdfghjklohhnhn <ethan.bartiromo@yahoo.com>
2015-09-22 17:23:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,7 +79,7 @@ var CHALLENGE = null;
 var BattleRoom = require('./battleroom');
 
 // The game type that we want to search for on startup
-var GAME_TYPE = (program.ranked) ? "randombattle" : "unratedrandombattle";
+var GAME_TYPE = (program.ranked) ? "OU" : "OU";
 
 // Load in Game Data
 var Pokedex = require("./data/pokedex");
